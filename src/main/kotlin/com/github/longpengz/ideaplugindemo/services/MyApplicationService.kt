de4e0f6021a82922dff6384843de157797e777b3
GitHub Action <action@github.com>
2022-05-23 06:59:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/longpengz/ideaplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/longpengz/ideaplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.longpengz.ideaplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.longpengz.ideaplugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/longpengz/ideaplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/longpengz/ideaplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.longpengz.ideaplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.longpengz.ideaplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
